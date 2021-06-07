1ee09c2b330d4103997ee264fecaded56f2f5835
李源琪 <45160501+liyuanqi501@users.noreply.github.com>
2021-06-07 10:31:13 +0800

    appium

---
+++ 李源琪/MainPageTest.java
@@ -0,0 +1,88 @@
+package testcase;
+
+import java.io.IOException;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import base.BaseDriver;
+import base.ExcelDataProvider;
+import pages.MainPage;
+
+public class MainPageTest extends BaseDriver{
+
+	MainPage mainPage;
+	@BeforeClass
+	public void login() {
+		mainPage=new MainPage(driver);
+	}
+	
+	@DataProvider(name="search")
+	public Object[][] searchTestdemo() throws IOException{
+		ExcelDataProvider data=new ExcelDataProvider();
+		return data.getTestDataByExcel("F://demo//LYQ//Username.xlsx", "Sheet1");
+	}
+	@Test(priority = 1,dataProvider="search")
+	public void test_createChat(String username) throws InterruptedException {
+		mainPage.createChat(username);
+	}
+	@Test(priority = 2,dataProvider="search")
+	public void test_createChat1(String username) throws InterruptedException {
+		mainPage.createChat1(username);
+	}
+	@Test(priority = 3,dataProvider="search")
+	public void test_createChat43(String username) throws InterruptedException {
+		mainPage.createChat3(username);
+	}
+	@Test(priority = 4,dataProvider="search")
+	public void test_createChat4(String username) throws InterruptedException {
+		mainPage.createChat4(username);
+	}
+	@Test(priority = 5,dataProvider="search")
+	public void test_createChat5(String username) throws InterruptedException {
+		mainPage.createChat5(username);
+	}
+	
+	@Test(priority = 6)
+	public void test_Collect() throws InterruptedException {
+		mainPage.Collect();
+	}
+	@Test(priority = 7)
+	public void test_DisCollect() throws InterruptedException {
+		mainPage.Discollect();
+	}
+	@Test(priority = 8)
+	public void test_searchData() throws InterruptedException {
+		mainPage.Search();
+	}
+	@Test(priority = 9)
+	public void test_searchDataH() throws InterruptedException {
+		mainPage.SearchH();
+		
+	}
+	@Test(priority = 10)
+	public void test_exSendmess1() throws InterruptedException {
+		mainPage.exSendMess2();
+	}
+	@Test(priority = 11)
+	public void test_collectGroup() throws InterruptedException {
+		mainPage.CollectGroup();
+	}
+	@Test(priority = 12)
+	public void test_disCollect() throws InterruptedException {
+		mainPage.disCollectGroup();
+	}
+	@Test(priority = 13)
+	public void test_searchGroupData() throws InterruptedException {
+		mainPage.searchMember();
+	}
+	@Test(priority = 14)
+	public void test_exSendmess2() throws InterruptedException {
+		mainPage.exSendMess1();
+	}
+//	@Test(priority = 15)
+//	public void test_exitGroup() throws InterruptedException {
+//		mainPage.exitGroup();
+//	}
+}
